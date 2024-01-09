0da1d19f3c0611fb015457d2444692beab01e25d
vaibhav0607 <vaibhavpunad02@gmail.com>
2024-01-09 17:26:48 +0530

    prims.java

---
+++ prims.java
@@ -0,0 +1,8 @@
+/*
+ 
+ Prims algo:
+ prims algo is used to find the min spamming tree
+ in a graph. it works 
+ by selecting an arbitrary node, and growing the tree outward from that point, adding node one
+ at a time until all nodes are included in the tree.
+ */
